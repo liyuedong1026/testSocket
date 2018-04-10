f59869054c52f0a5e574b0ff470385f1b0f245eb
liyuedong <liyuedong@pset.suntec.net>
2018-04-10 18:08:14 +0800

    senddata
    Change-Id: I713640cdf6bc0168582d0c6c8835727b819d562a

--- testClient/mainwindow.cpp
+++ testClient/mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileInfo>
+#include <QFileDialog>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -23,6 +25,8 @@ MainWindow::MainWindow(QWidget *parent) :
             qDebug() << "Connect fail!";
         }
     }
+
+    connect(&m_timer, SIGNAL(timeout()), this, SLOT(startSendData()));
 }
 
 MainWindow::~MainWindow()
@@ -34,7 +38,57 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::sendData()
+{
+    qDebug() << "sendData";
+    if (NULL == m_client) {
+        return;
+    }
+
+    qDebug("size = %lld", m_filesize);
+    qint64 len = 0;
+    do {
+        char buf[4*1024] = {0};
+        len = 0;
+        len = m_file.read(buf,sizeof(buf));
+        len = m_client->write(buf,len);
+        m_sendsize += len;
+    } while(len > 0);
+}
+
+void MainWindow::startSendData()
+{
+    qDebug() << "startSendData";
+    m_timer.stop();
+    sendData();
+}
+
 void MainWindow::on_btn_send_pressed()
 {
+    qDebug() << "on_btn_send_pressed";
+    QString head = QString("%1##%2").arg(m_filename).arg(m_filesize);//自定义文件头部
 
+     //发送头部信息
+    qint64 len = m_client->write(head.toUtf8());
+    if(len > 0)//头部信息发送成功
+    {
+        m_timer.start(20);
+    }
+}
+
+void MainWindow::on_btn_openfile_pressed()
+{
+    QString filepath = QFileDialog::getOpenFileName(this,"open","../");
+
+    if(!filepath.isEmpty())
+    {
+        m_filename.clear();
+        m_filesize = 0;
+        QFileInfo info(filepath);
+        m_filename = info.fileName();
+        m_filesize = info.size();
+        m_sendsize = 0;
+        m_file.setFileName(filepath);
+    }
+    m_file.open(QIODevice::ReadOnly);
 }
--- testClient/mainwindow.h
+++ testClient/mainwindow.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 #include <QTcpSocket>
+#include <QTimer>
+#include <QFile>
 
 namespace Ui {
 class MainWindow;
@@ -15,15 +17,27 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void sendData();
+
+
+public slots:
+    void startSendData();
 
 private slots:
     void on_btn_send_pressed();
 
+    void on_btn_openfile_pressed();
+
 private:
     Ui::MainWindow *ui;
     QTcpSocket *m_client;
+    QTimer m_timer;
     QString m_strIP;
     int m_port;
+    QFile m_file;
+    QString m_filename;
+    qint64 m_filesize;
+    qint64 m_sendsize;
 };
 
 #endif // MAINWINDOW_H
